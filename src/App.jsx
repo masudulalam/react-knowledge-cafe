4ca9912783723d30fb7d7d0426c777fa238b2c93
masudulalam <masuudulalam@gmail.com>
2025-01-03 09:52:21 +0600

    created fake data in blogs.json component and load the fake data in blogs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/blogs/Blogs'
 import Headers from './components/header/Headers'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Headers></Headers>
+      <Blogs></Blogs>
     </>
   )
 }
